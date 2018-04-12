2475c92926c00afcc19a8dc6ec516e26c58eedfc
Slava <vladimirzhbanko@MacBook-Pro-Vladimir.local>
2018-04-12 14:45:23 +0200

    README and Lektion_2 finished.

--- Lektion_2.ino
+++ Lektion_2.ino
@@ -11,6 +11,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   switchState = digitalRead(2);
+
+  if (switchState == LOW) {
+    //il pulsante è premuto
+    digitalWrite(3, HIGH); // LED verde
+    digitalWrite(4, LOW); // LED rosso
+    digitalWrite(5, LOW); // LED rosso
 }
 
-  if (switchState == LOW) 
+else { // il pulsante è premuto
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  delay(250); // aspetta un quarto di secondo
+  // cambia gli stati dei LED
+  digitalWrite(4 HIGH);
+  digitalWrite(5, LOW);
+  delay(250); // aspetta un quarto di secondo
+  }
+} // torna indietro all'inizio del loop
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Arduino_Lektion_2
 Erste Lektion über Arduino.
+LOW means there is no current;
+HIGH mean there is current;
